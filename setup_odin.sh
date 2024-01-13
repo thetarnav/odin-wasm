01ae5264c98ed949544cef24faa2725e06f3da2a
Damian Tarnawski <gthetarnav@gmail.com>
2024-01-13 15:59:33 +0100

    Make a bash script to install Odin

---
+++ setup_odin.sh
@@ -0,0 +1,15 @@
+ODIN_RELEASE_ID=dev-2024-01
+
+echo "Setting up LLVM 14 and Odin ${ODIN_RELEASE_ID}..."
+
+wget -O odin.zip https://github.com/odin-lang/Odin/releases/download/${ODIN_RELEASE_ID}/odin-ubuntu-amd64-${ODIN_RELEASE_ID}.zip
+unzip odin.zip -d /home/runner/odin
+chmod +x /home/runner/odin/odin
+
+echo "/home/runner/odin" >>$GITHUB_PATH
+echo "/usr/lib/llvm-14/bin" >>$GITHUB_PATH
+
+echo "Done!"
+echo "llvm: $(llvm-config --version)"
+echo "wasm-ld: $(wasm-ld --version)"
+echo "odin: $(odin version)"
