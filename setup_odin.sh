8f18ca680b2914bad2a2ae4efb62c8b2a0287938
Damian Tarnawski <gthetarnav@gmail.com>
2024-01-13 16:07:02 +0100

    Cleanup setup_odin.sh

--- setup_odin.sh
+++ setup_odin.sh
@@ -2,7 +2,7 @@ ODIN_RELEASE_ID=dev-2024-01
 
 echo "Setting up LLVM 14 and Odin ${ODIN_RELEASE_ID}..."
 
-wget -O odin.zip https://github.com/odin-lang/Odin/releases/download/${ODIN_RELEASE_ID}/odin-ubuntu-amd64-${ODIN_RELEASE_ID}.zip
+wget -O odin.zip -q https://github.com/odin-lang/Odin/releases/download/${ODIN_RELEASE_ID}/odin-ubuntu-amd64-${ODIN_RELEASE_ID}.zip
 unzip odin.zip -d /home/runner/odin
 chmod +x /home/runner/odin/odin
 
@@ -12,6 +12,6 @@ export PATH="/home/runner/odin:$PATH"
 export PATH="/usr/lib/llvm-14/bin:$PATH"
 
 echo "Done!"
-echo "llvm: $(llvm-config --version)"
+echo "llvm:    $(llvm-config --version)"
 echo "wasm-ld: $(wasm-ld --version)"
-echo "odin: $(odin version)"
+echo "odin:    $(odin version)"
