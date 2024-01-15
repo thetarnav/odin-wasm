ccc7456a66ecd072e94381ead35e9fb7d95267e2
Damian Tarnawski <gthetarnav@gmail.com>
2024-01-15 22:33:03 +0100

    Cleanup

--- example/shader_fragment.glsl
+++ example/shader_fragment.glsl
@@ -6,6 +6,5 @@ precision mediump float;
 varying vec4 v_color;
 
 void main() {
-  // gl_FragColor is a special variable a fragment shader is responsible for setting
-    gl_FragColor = v_color;
+	gl_FragColor = v_color;
 }
--- example/shader_vertex.glsl
+++ example/shader_vertex.glsl
@@ -8,13 +8,9 @@ varying vec4 v_color;
 
 void main() {
     // from pixels to 0->1 then to 0->2 then to -1->+1 (clipspace)
-    vec2 clip_space = (a_position / u_resolution) * 2.0 - 1.0;
+	vec2 clip_space = (a_position / u_resolution) * 2.0 - 1.0;
 
-    gl_Position = vec4(clip_space * vec2(1, -1), 0, 1);
+	gl_Position = vec4(clip_space * vec2(1, -1), 0, 1);
 
-    // Convert from clip space to color space.
-    // Clip space goes -1.0 to +1.0
-    // Color space goes from 0.0 to 1.0
-    // v_color = 1.0 - (gl_Position * 0.5 + 0.5);
-    v_color = a_color;
+	v_color = a_color;
 }
