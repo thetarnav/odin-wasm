8436661571605809a2855e8941189621f0abe7db
Damian Tarnawski <gthetarnav@gmail.com>
2024-04-18 22:14:05 +0200

    Clamp light limit

--- example/spotlight_fs.glsl
+++ example/spotlight_fs.glsl
@@ -13,26 +13,31 @@ uniform vec3 u_light_two_direction;
 
 out vec4 out_color;
 
-#define limit 0.92
+#define limit_start 0.9
+#define limit_range 0.1
+
+/*
+float step(a, b) {
+	return a < b ? 1.0 : 0.0;
+}
+*/
+
+float light_strength(vec3 normal, vec3 surface_to_light, vec3 light_direction) {
+	float dot_in_light  = dot(surface_to_light, -light_direction);
+	float in_light      = clamp((dot_in_light - limit_start) / limit_range, 0.0, 1.0);
+	float dot_in_normal = dot(normal, surface_to_light);
+
+	return in_light * (dot_in_normal - 0.35) * 2.0;
+}
 
 void main() {
 	// varrying variables are interpolated
 	vec3 normal               = normalize(v_normal);
 	vec3 surface_to_light_one = normalize(v_surface_to_light_one);
 	vec3 surface_to_light_two = normalize(v_surface_to_light_two);
-
-	/*
-	float step(a, b) {
-		return a < b ? 1.0 : 0.0;
-	}
-	*/
-	float light_one =
-		step(limit, dot(surface_to_light_one, -u_light_one_direction)) *
-		(dot(normal, surface_to_light_one) - 0.35) * 2.0;
-
-	float light_two =
-		step(limit, dot(surface_to_light_two, -u_light_two_direction)) *
-		(dot(normal, surface_to_light_two) - 0.35) * 2.0;
+	
+	float light_one = light_strength(normal, surface_to_light_one, u_light_one_direction);
+	float light_two = light_strength(normal, surface_to_light_two, u_light_two_direction);
 
 	vec4 white = vec4(1.0, 1.0, 1.0, 1.0);
 	out_color = (u_light_one_color * light_one + u_light_two_color * light_two);
